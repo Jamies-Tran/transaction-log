2302074d9990b9b4554453fd6e733d6cd6322fc8
minhtq <minhtranquang2197@gmail.com>
2026-04-06 20:56:09 +0700

    first commit

---
+++ src/main/java/com/example/transactionlog/TransactionLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionLogApplication.class, args);
+    }
+
+}
